f9f3d169c70699b85cde90ab8e016a9fcdb05122
mukuru1 <kabengaedwin@gmail.com>
2026-04-24 13:11:10 +0200

    load data

--- fullstack/SupportFlow-Visual-Builder/src/utils/graph.js
+++ fullstack/SupportFlow-Visual-Builder/src/utils/graph.js
@@ -5,3 +5,10 @@ export function getOutgoingConnections(connections, nodeId) {
 export function getIncomingConnections(connections, nodeId) {
   return connections.filter((c) => c.targetId === nodeId);
 }
+
+export function getChildren(nodes, connections, nodeId) {
+  const outgoing = getOutgoingConnections(connections, nodeId);
+  return outgoing
+    .map((c) => nodes.find((n) => n.id === c.targetId))
+    .filter(Boolean);
+}

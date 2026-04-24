9b8b6c0b5ca6f6b00a8420a66e504c1a25d8bae3
mukuru1 <kabengaedwin@gmail.com>
2026-04-24 17:35:33 +0200

    connections

--- fullstack/SupportFlow-Visual-Builder/src/utils/coordinates.js
+++ fullstack/SupportFlow-Visual-Builder/src/utils/coordinates.js
@@ -9,3 +9,25 @@ export function getNodeCenter(node) {
     y: node.position.y + NODE_HEADER_HEIGHT / 2,
   };
 }
+
+export function getOutputPortPosition(node) {
+  return {
+    x: node.position.x + NODE_WIDTH / 2,
+    y: node.position.y + getNodeHeight(node),
+  };
+}
+
+export function getInputPortPosition(node) {
+  return {
+    x: node.position.x + NODE_WIDTH / 2,
+    y: node.position.y,
+  };
+}
+
+export function getOptionPortPosition(node, optionIndex, totalOptions) {
+  const spacing = NODE_WIDTH / (totalOptions + 1);
+  return {
+    x: node.position.x + spacing * (optionIndex + 1),
+    y: node.position.y + getNodeHeight(node),
+  };
+}
